a4ba2316593abbc7c6fbe9018b323f347e5a79e3
Denis Doktorov <denisd581@gmail.com>
2021-04-19 03:46:30 +0300

    Fix PEP8 errors v2.0

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     email = models.EmailField(
         unique=True,
         db_index=True,
-        verbose_name='Email',   
+        verbose_name='Email',
     )
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -40,19 +40,19 @@ class User(AbstractUser):
     is_active = models.BooleanField(default=False)
     bio = models.TextField(blank=True, null=True)
     first_name = models.CharField(
-        max_length=200, 
+        max_length=200,
         blank=True,
         null=True,
         verbose_name='Имя',
     )
     last_name = models.CharField(
-        max_length=200, 
+        max_length=200,
         blank=True,
         null=True,
         verbose_name='Фамилия',
     )
     confirmation_code = models.CharField(
-        max_length=200, 
+        max_length=200,
         blank=True,
         null=True,
         verbose_name='Код подтверждения',
